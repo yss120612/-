a9b84bbc8139faedcffb11eae24a68132e905f09
yss120612 <yss1@yandex.ru>
2017-10-28 11:04:20 +0800

    home2

---
+++ src/main/java/com/yss1/pproc/Man.java
@@ -0,0 +1,35 @@
+package com.yss1.pproc;
+
+public class Man {
+private String name;
+private String fam;
+private String otch;
+private String snils;
+
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getFam() {
+	return fam;
+}
+public void setFam(String fam) {
+	this.fam = fam;
+}
+public String getOtch() {
+	return otch;
+}
+public void setOtch(String otch) {
+	this.otch = otch;
+}
+public String getSnils() {
+	return snils;
+}
+public void setSnils(String snils) {
+	this.snils = snils;
+}
+
+
+}
