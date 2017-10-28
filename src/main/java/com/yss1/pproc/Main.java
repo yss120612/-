d53b633c2fcbdbaf411200d9fce6619ba588564e
yss120612 <yss1@yandex.ru>
2017-10-28 11:00:17 +0800

    home1

--- src/main/java/com/yss1/pproc/Main.java
+++ src/main/java/com/yss1/pproc/Main.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+import com.yss1.pproc.util.Utils;
+
 public class Main extends SpringBootServletInitializer {
 
 	 private static Class<Main> applicationClass = Main.class;
 	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Man m=new Man();
+		System.out.println(Utils.calcSNILS("174672541"));
+		System.out.println(Utils.calcSNILS("046884586"));
 		SpringApplication.run(applicationClass, args);
 	}
 	
