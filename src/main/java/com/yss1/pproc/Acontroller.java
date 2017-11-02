d829305b7d9b1ddf2d7c32a8cfe4eabce43ca351
yss120612 <yss1@yandex.ru>
2017-11-02 23:17:58 +0800

    123

--- src/main/java/com/yss1/pproc/Acontroller.java
+++ src/main/java/com/yss1/pproc/Acontroller.java
@@ -1,21 +1,12 @@
 package com.yss1.pproc;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class Acontroller {
